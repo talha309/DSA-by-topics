2fd34a9e3d750a3716b13009ef937f08343203aa
talha309 <tiseoexpert@gmail.com>
2025-11-01 09:04:41 +0500

    image memory address

---
+++ 01_pointer/1_memory_address.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << "Address of a: " << &a << endl; 
+    // The memory address always a hexa decimal number (16)
+    cout << "Value of a:" << a <<endl;
+
+    float num = 3.14;
+    cout <<"Address of num: "<< &num << endl;
+    cout << "Value of num:" << num <<endl;
+
+    double dnum = 9.81;
+    cout <<"Address of dnum: "<< &dnum << endl;
+    cout << "Value of dnum:" << dnum <<endl;
+
+    char ch = a;
+    cout << "Address of ch: " << (void *)&ch << endl; // cast to void*
+    cout << "Value of ch:" << ch <<endl;
+    return 0;
+}
